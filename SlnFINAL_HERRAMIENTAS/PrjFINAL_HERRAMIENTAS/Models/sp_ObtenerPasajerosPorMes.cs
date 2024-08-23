cdc541a1fefc8a93e7d69fb3ff48d5a45ed3c8d6
Gustavo Valera <gvaleramendoza@gmail.com>
2024-08-23 11:51:31 -0500

    viaje cliente

--- SlnFINAL_HERRAMIENTAS/PrjFINAL_HERRAMIENTAS/Models/sp_ObtenerPasajerosPorMes.cs
+++ SlnFINAL_HERRAMIENTAS/PrjFINAL_HERRAMIENTAS/Models/sp_ObtenerPasajerosPorMes.cs
@@ -5,7 +5,6 @@ namespace PrjFINAL_HERRAMIENTAS.Models
     public class sp_ObtenerPasajerosPorMes
     {
         [Key]
-
         public string? nro_via { get; set; }  // Cambia el nombre de la propiedad
         public int TotalPasajeros { get; set; }
     }
